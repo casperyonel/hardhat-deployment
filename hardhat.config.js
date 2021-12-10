cee8dc734859930e8143189cffd642c980b7809e
Casper Yonel <yonelcasper@gmail.com>
2021-12-09 19:38:31 -0700

    updated with env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -24,6 +25,10 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/11b103281c6f48f9802389e46064fd1b",
+      accounts: [`0x${process.env.ACCOUNT_PRIVATE_KEY}`]
     }
   }
 };

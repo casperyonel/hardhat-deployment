28a643d0dc5cbac6663f22c0c8e03132541ded31
Casper Yonel <yonelcasper@gmail.com>
2021-12-09 18:32:31 -0700

    working with local blockchain

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { ethers } from 'ethers';
-import { Greeter } from './artifacts/contracts/Greeter.sol/Greeter.json'
-import { EtherscanProvider } from '@ethersproject/providers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 
 const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
-
 function App() {
-
   const [greeting, setGreetingValue] = useState('')
 
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' })
   } // Prompts user to connect metamask account. 
 
+  // GETTING DATA FROM A SMART CONTRACT ON THE NETWORK: 
   async function fetchGreeting() {
     if (typeof window.ethereum !== 'undefined') { // Make sure they have metamask extension
       const provider = new ethers.providers.Web3Provider(window.ethereum) // Web3Provider is just one provider we can use
@@ -24,44 +21,34 @@ function App() {
         const data = await contract.greet() // Calling method in contract
         console.log('data: ' , data)
        } catch (err) {
-         console.log("Error: ", err)
+         console.log("Error: " , err)
        }
     }
   }
 
+  // WRITING TO THE NETWORK AND UPDATING BLOCKCHAIN: 
   async function setGreeting() {
     if (!greeting) return // Ensures state is not blank, they wrote something in box. 
     if (typeof window.ethereum !== 'undefined') { // Make sure they have metamask extension
-      await requestAccount() // Prompts user to connect metamask account.
+      await requestAccount() // Prompts user to connect metamask wallet.
       const provider = new ethers.providers.Web3Provider(window.ethereum) 
       const signer = provider.getSigner() // Sign into metamask
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer) // USING SIGNER ADDRESS INSTEAD OF PROVIDER NOW
       const transaction = await contract.setGreeting(greeting) // Passing in state to setGreeting method in contract we accessed
-      await transaction.wait() 
-      fetchGreeting() // To display
+      setGreetingValue('') // Resetting state since we already passed greeting to contract
+      await transaction.wait() // Waits for it to be written to blockchain
+      fetchGreeting() // To display, since this calls the greeting() method which actually returns our greeting to user. 
     }
   }
 
 
   return (
-
-
-
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input type="text" onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+
     </div>
   );
 }
